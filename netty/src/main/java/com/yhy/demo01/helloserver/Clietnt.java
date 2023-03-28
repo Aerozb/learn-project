68e7acf9abbb4f1c1585281317435a6f9bf8a45a
Aerozb <847888663@qq.com>
2023-03-28 22:52:54 +0800

    first commit

---
+++ netty/src/main/java/com/yhy/demo01/helloserver/Clietnt.java
@@ -0,0 +1,29 @@
+package com.yhy.demo01.helloserver;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class Clietnt {
+    public static void main(String[] args) throws InterruptedException {
+        ChannelFuture channelFuture = new Bootstrap().group(new NioEventLoopGroup()).channel(NioSocketChannel.class).handler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new StringEncoder());
+            }
+        }).connect(new InetSocketAddress("localHost", 8080));
+        //ChannelFuture sync = channelFuture.sync();
+        //Channel channel = sync.channel();
+        //channel.writeAndFlush("牛逼");
+
+        channelFuture.addListener((ChannelFutureListener) future -> {
+            ChannelFuture sync = channelFuture.sync();
+            Channel channel = sync.channel();
+            channel.writeAndFlush("牛逼");
+        });
+    }
+}
---
+++ netty/src/main/java/com/yhy/demo01/helloserver/Server.java
@@ -0,0 +1,51 @@
+package com.yhy.demo01.helloserver;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Server {
+
+    public static void main(String[] args) {
+
+        new ServerBootstrap().
+                group(new NioEventLoopGroup()).
+                channel(NioServerSocketChannel.class).
+                childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.info("2");
+//                                super.channelRead(ctx, msg);
+                                ch.writeAndFlush("yyyyy");
+                            }
+                        });
+                        ch.pipeline().addLast(new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.info("3");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                }).bind(8080);
+    }
+}
