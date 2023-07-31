a88e4f593ac8db03124edf043adf31a3fd1f56e0
aerozb <847888663@qq.com>
2023-07-31 18:29:08 +0800

    自定义SpringBootStarter

---
+++ custom_springboot_start/demo/src/main/java/com/demo/DemoConfig.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DemoConfig {
+
+    @Value("${demo}")
+    private String demo;
+
+    @Bean
+    public DemoService demoService(){
+        return new DemoService(demo);
+    }
+}
---
+++ custom_springboot_start/demo/src/main/java/com/demo/DemoService.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class DemoService {
+
+
+    private String demo;
+
+    public DemoService(String demo) {
+        this.demo = demo;
+    }
+
+    public String getService(){
+        return demo;
+    }
+}
+
